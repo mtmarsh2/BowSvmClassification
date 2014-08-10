ee130d24cc869fb0ae525a250f862f588e8ebcfc
Mike Marshall <User1@Michaels-MacBook-Pro-3.local>
2014-08-09 23:19:02 -0400

    Added load from file capability

--- classifyCatvsDog.py
+++ classifyCatvsDog.py
@@ -10,6 +10,7 @@ import cPickle as pickle
 import numpy
 from sklearn import svm
 import random
+import os.path
 """
 1. Read in images from folder
 2. Find sift descriptors on all
@@ -25,8 +26,8 @@ num_clusters = 300
 image_to_descriptors = {}
 list_of_image_names = glob.glob(os.path.join(folder_name, "*.jpg"))
 all_image_histograms = {}
-kmeans = MiniBatchKMeans(n_clusters = 1000, batch_size = 1000, max_iter = 250)
-
+kmeans = MiniBatchKMeans(n_clusters = num_clusters, batch_size = 100, max_iter = 250)
+sift_descriptors_filename = "sift_descriptors.pickle"
 def count_type_in_list(mylist, mytype):
      return len([elem for elem in mylist if os.path.basename(elem).split(".")[0] == mytype ])
 
@@ -53,8 +54,8 @@ def create_labels_matrix(image_names):
 def build_dataset(list_of_image_names):
      image_to_descriptors = {}
      for index,image_name in enumerate(list_of_image_names):
-          if index % 10 == 0:
-               image_to_descriptors[image_name] = get_sift_descriptors(image_name)
+          if index % 1000 == 0: print index
+          image_to_descriptors[image_name] = get_sift_descriptors(image_name)
      return image_to_descriptors
 
 #dumps descriptors to file
@@ -81,30 +82,38 @@ def test_classification(predicted_labels, trained_labels):
                total_correct += 1
      return total_correct / len(predicted_labels)
 
-if __name__ == "__main__":
+
+#sift descriptors part
+if os.path.isfile(sift_descriptors_filename ):
+     print "Loading sift descriptors"
+     image_to_descriptors = load_sift_descriptors_from_file(sift_descriptors_filename )
+else:
+     print "Creating sift descriptors from images and dumping to file"
      image_to_descriptors = build_dataset(list_of_image_names)
-     #make a list of all sift descriptors to be fed into kmeans
-     list_of_sift_descriptors = np.vstack(image_to_descriptors.values())
+     dump_sift_descriptors_to_file(sift_descriptors_filename , image_to_descriptors)
 
-     #setup kmeans
-     print "Setting up kmeans"
-     kmeans.fit(list_of_sift_descriptors)
-     #create bow histograms for images
-     for index,image_name in enumerate(list_of_image_names):
-          if index:
-               image_to_descriptors[image_name] = get_sift_descriptors(image_name)
-               labels = kmeans.predict(image_to_descriptors[image_name])
-               all_image_histograms[image_name] = create_histogram(labels)
+#make a list of all sift descriptors to be fed into kmeans
+list_of_sift_descriptors = np.vstack(image_to_descriptors.values())
 
-     correct_labels = create_labels_matrix(all_image_histograms.keys())
+#setup kmeans
+print "Setting up kmeans"
+kmeans.fit(list_of_sift_descriptors)
+#create bow histograms for images
+for index,image_name in enumerate(list_of_image_names):
+     if index % 1000 ==0: print index
+          #image_to_descriptors[image_name] = get_sift_descriptors(image_name)
+     labels = kmeans.predict(image_to_descriptors[image_name])
+     all_image_histograms[image_name] = create_histogram(labels)
 
-     ###SVM Training
-     print "Training svm"
-     clf = svm.SVC()
-     clf.fit(all_image_histograms.values(), correct_labels)
+correct_labels = create_labels_matrix(all_image_histograms.keys())
 
-     print "Predicting labels"
-     predicted_labels = classify_images(clf, list_of_image_names)
-     trained_labels = create_labels_matrix(list_of_image_names)
-     accuracy = test_classification(predicted_labels, trained_labels)
-     print accuracy
+###SVM Training
+print "Training svm"
+clf = svm.SVC()
+clf.fit(all_image_histograms.values(), correct_labels)
+
+print "Predicting labels"
+predicted_labels = classify_images(clf, list_of_image_names)
+trained_labels = create_labels_matrix(list_of_image_names)
+accuracy = test_classification(predicted_labels, trained_labels)
+print accuracy
